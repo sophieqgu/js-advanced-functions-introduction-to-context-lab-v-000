dac34b84177c329416d1ac7700dd13a330fc8826
Sophie Gu <sophieqgu@gmail.com>
2021-01-22 04:45:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+function createEmployeeRecord(firstName, familyName, title, payPerHour) {
+  return Object.assign({}, {
+    firstName = firstName;
+    familyName = familyName;
+    title = title;
+    payPerHour = payPerHour;
+    timeInEvents = [];
+    timeOutEvents = [];
+  });
+}
+
+function createEmployeeRecords(employees) {
+  return employees.map(createEmployeeRecord);
+}
